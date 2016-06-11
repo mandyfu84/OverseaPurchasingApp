0590d1d3760fcfc0008573da0de75965c06fbf01
Mandy Fu <mandyfu84@gmail.com>
2016-06-11 15:30:38 +0800

    add AddItemTableViewController

---
+++ OverseaPurchasing/AddItemTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddItemTableViewController.h
+//  OverseaPurchasing
+//
+//  Created by Fu Juo Wen on 2016/6/11.
+//  Copyright © 2016年 ntnu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemTableViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UITextField *ItemName;
+@property (strong, nonatomic) IBOutlet UITextField *ItemPrice;
+@property (strong, nonatomic) IBOutlet UITextField *ItemCategory;
+@property (strong, nonatomic) IBOutlet UITextField *ItemLocation;
+@property (strong, nonatomic) IBOutlet UITextField *ItemDetail;
+- (IBAction)TakeaPhoto:(id)sender;
+
+@end
