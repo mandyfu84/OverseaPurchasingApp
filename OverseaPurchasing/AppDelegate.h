77bf1cd27fd2e12cd7929c5f372acf0c509d77af
gilesabc <gilesabc@gmail.com>
2016-06-14 13:43:56 +0800

    fh

--- OverseaPurchasing/AppDelegate.h
+++ OverseaPurchasing/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Fu Juo Wen on 2016/6/4.
 //  Copyright © 2016年 ntnu. All rights reserved.
 //
-// test
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
